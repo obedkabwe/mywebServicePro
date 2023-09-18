febb7e7768273869fdf8d7b3a6274110fac69a22
obedkabwe <kabweobed@gmail.com>
2023-09-17 19:15:31 -0500

    entites are CREATED

---
+++ src/main/java/com/oberson/resources/UserResource.java
@@ -0,0 +1,5 @@
+package com.oberson.resources;
+
+public class UserResource {
+
+}
---
+++ src/test/java/com/oberson/WebserviceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oberson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
